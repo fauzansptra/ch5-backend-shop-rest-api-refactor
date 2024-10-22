7293fbd58e7540974577bf2add460825a39c38bd
xkylz1 <fauzansaputra741@gmail.com>
2024-10-22 23:00:14 +0800

    selasa

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
+const cors=require("cors")
 
 const router = require("./routes");
 const { systemController } = require("./controllers");
 
 const app = express();
 
+app.use(cors())//wildcard kalo ga da isinya
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); //buat view engine
+
+app.use(morgan("dev"))
 
 app.get("/api/v1/health-check", systemController.healtcheck);
 app.use("/api/v1", router);
